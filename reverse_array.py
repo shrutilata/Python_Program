46b480d56950a6d1e27d7ffd7276fec2c905d595
shrutilata <63225735+shrutilata@users.noreply.github.com>
2020-04-29 21:13:44 +0530

    Update reverse_array.py

--- reverse_array.py
+++ reverse_array.py
@@ -1,6 +1,23 @@
-n= int(input("Enter the no. of elements in an array"))
-l=[]
-for i in range(0,n):
-    a=int(input("Enter the no.of elements"))
-    l.append(a)
-    print(*l[::-1])    
+#Source Code:--
+
+def Reverse_array(n):
+    l=[]
+    for i in range(0,n):
+        a=int(input("Enter the elements:--"))
+        l.append(a)
+    print("The reversed array is",*l[::-1])     
+
+#Driver Code:--
+Reverse_array(7)
+
+#Output
+
+Enter the elements:--1
+Enter the elements:--2
+Enter the elements:--3
+Enter the elements:--4
+Enter the elements:--5
+Enter the elements:--6
+Enter the elements:--7
+    
+The reversed array is 7 6 5 4 3 2 1
